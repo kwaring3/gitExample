265e3831a4b33c1c15ebd414478158f7ab0ce637
kwaring3 <kevinwaring@pursuit.org>
2019-02-08 10:54:53 -0500

    First Commit

---
+++ getExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  getExample
+//
+//  Created by Kevin Waring on 2/8/19.
+//  Copyright © 2019 Kevin Waring. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("git git")
+    }
+
+
+}
+
